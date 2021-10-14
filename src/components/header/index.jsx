9f4159b75a327757ef57a900736aebd4d647dfc9
n.s.mordvinov <n.s.mordvinov@gmail.com>
2021-10-14 14:45:53 +0400

    added colors for the header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import { Person, Loupe, Arrow, Burger } from '../../components/ui/Icons/';
 import { Button } from '../../components/ui/Button';
 import { Menu } from './menu';
@@ -27,11 +28,11 @@ export const Header = ({ logo }) => {
   return (
     <header className={ style.header }>
       <div className={ style.headerContainer }>
-        <Button clear className={ style.headerBtn }><Arrow className={ style.headerBtn } /></Button>
-        <Button clear className={ style.headerBtn } onClick={ onMenuToggle }><Burger /></Button>
+        <Button clear className={ style.headerBtn }><Arrow className={ style.headerBtnIcon } /></Button>
+        <Button clear className={ style.headerBtn } onClick={ onMenuToggle }><Burger className={ style.headerBtnIcon } /></Button>
         <h1 className={ style.headerTitle }>Members</h1>
-        <Button clear className={ style.headerBtn }><Loupe /></Button>
-        <Button clear className={ style.headerBtn }><Person /></Button>
+        <Button clear className={ style.headerBtn }><Loupe className={ style.headerBtnIcon } /></Button>
+        <Button clear className={ style.headerBtn }><Person className={ style.headerBtnIcon } /></Button>
       </div>
       <Menu open={ menuOpen } onClose={ onMenuToggle } />
     </header>
