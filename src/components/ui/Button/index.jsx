dc7418ae3cfc6f628ba6bff66cdee72496bdea80
n.s.mordvinov <n.s.mordvinov@gmail.com>
2021-10-21 09:46:25 +0400

    changed button styles

--- src/components/ui/Button/index.jsx
+++ src/components/ui/Button/index.jsx
@@ -2,11 +2,19 @@ import cn from 'classnames'
 
 import styles from './styles.module.scss';
 
-export const Button = ({ children, onClick, primary, clear, className, ...allProps }) => {
-  const classPrimary = primary ? styles.btnPrimary : '';
-  const classClear = clear ? styles.btnClear : '';
+export const Button = ({ children, type, onClick, outline, primary, clear, className, ...allProps }) => {
+  const classOutline = outline ? styles.buttonOutline : null;
+  const classPrimary = primary ? styles.buttonPrimary : null;
+  const classClear = clear ? styles.buttonClear : null;
 
   return (
-    <button className={ cn(styles.btn, classPrimary, classClear, className) } onClick={onClick} { ...allProps }>{ children }</button>
+    <button
+      type={ type ? type : 'button' }
+      onClick={ onClick }
+      className={ cn(styles.button, classOutline, classPrimary, classClear, className) }
+      { ...allProps }
+    >
+      { children }
+    </button>
   )
 }
