e4cbe3683dddc498a1f6114258849a3168a8cca6
n.s.mordvinov <n.s.mordvinov@gmail.com>
2021-10-25 13:31:29 +0400

    fix avatar

--- src/components/avatar/index.jsx
+++ src/components/avatar/index.jsx
@@ -4,13 +4,27 @@ import { ReactComponent as GoneSvg } from './svg/gone.svg';
 import style from './styles.module.scss';
 
 export const Avatar = ({ size, className, type, data }) => {
-  const classNameSize = size === 'big' ? style.avatarBig : null;
-  const classNameType = type === 'invited' ? style.avatarInvited : type === 'active' ? style.avatarActive : null;
+  const classNameSize = size === 'big' ?  style.avatarBig 
+    : size === 'small' ? style.avatarSmall
+    : null;
+
+  const classNameType = type === 'invited' ? style.avatarInvited
+    : type === 'active' ? style.avatarActive
+    : type === 'facility' ? style.avatarFacility
+    : null;
+
+  const transformLetters = (value) => {
+    if(value.length > 2) {
+      const str = value.split(' ');
+      return str[0][0] + str[1][0];
+    }
+    return value;
+  }
 
   return (
     <div className={ cn(style.avatar, classNameSize, classNameType, className) }>
-      <div className={ style.avatarText }>{ data }</div>
-      { type !== 'active' ? <GoneSvg className={ style.avatarIcon } /> : null }
+      <div className={ style.avatarText }>{ transformLetters(data) }</div>
+      { type !== 'active' && type !== 'facility' ? <GoneSvg className={ style.avatarIcon } /> : null }
     </div>
   )
 }
