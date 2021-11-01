11b8062abb39e9801ebcee7fdee2f81c41c93aaf
n.s.mordvinov <n.s.mordvinov@gmail.com>
2021-11-01 14:38:11 +0400

    added memberItemActionBlock

--- src/components/members/members-item/index.jsx
+++ src/components/members/members-item/index.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+import cn from 'classnames';
 import { Button } from '../../ui/Button';
 import { Actions } from '../../ui/Icons';
 import { Avatar } from '../../avatar';
 import style from './styles.module.scss';
 
 export const MembersItem = ({ data }) => {
+  const [visibleActions, setVisibleActions] = useState(false);
+
+  const toggleVisibleActions = () => {
+    setVisibleActions(!visibleActions);
+  }
+
   return (
     <div className={ style.membersItem }>
       <Avatar type="active" data={ data.avatar } />
@@ -12,7 +20,16 @@ export const MembersItem = ({ data }) => {
         <div className={ style.membersItemPermissions }>{ data.position }</div>
       </div>
       <div className={ style.membersItemActions }>
-        <Button clear className={ style.membersItemActionsBtn }>
+        <div className={ cn(style.membersItemActionsBlock, visibleActions ? style.membersItemActionsBlockVisible : null) }>
+          <div className={ style.membersItemActionsBlockLink }>Edit name</div>
+          <div className={ style.membersItemActionsBlockLink }>Edit position</div>
+          <div className={ style.membersItemActionsBlockLink }>Deactivated</div>
+        </div>
+        <Button
+          clear
+          className={ style.membersItemActionsBtn }
+          onClick={ toggleVisibleActions }
+        >
           <Actions />
         </Button>
       </div>
