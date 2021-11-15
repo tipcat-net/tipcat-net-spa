4086d9488c8e6120512a78f3bc16da43af8aa588
n.s.mordvinov <n.s.mordvinov@gmail.com>
2021-11-15 17:37:46 +0400

    logout

--- src/pages/Logout/index.jsx
+++ src/pages/Logout/index.jsx
@@ -1,38 +1,43 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
-import { useTranslation } from "react-i18next";
+import { useTranslation } from 'react-i18next';
 
-import { Header } from './../../components/header';
 import Spinner from "../../components/spinner";
-import { Button } from "../../components/ui/Button";
+import { Button } from '../../components/ui/Button';
+import { Logo } from '../../components/ui/Icons';
 
+import { authConfig } from '../../auth0/auth0-config';
 import { ROUTES } from "../../constants/routes";
 
 import style from './styles.module.scss';
 
 export const Logout = () => {
   const { t } = useTranslation();
-  const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();
 
   if (isLoading) {
     return <Spinner />
   }
 
-  if (!isAuthenticated) {
-    return (
-      <Redirect
-        to={ ROUTES.AUTH.path }
-      />
-    )
+  if (isAuthenticated) {
+    logout({ returnTo: authConfig.logoutUri })
   }
 
   return (
     <div className={ style.logout }>
-      <Header logo/>
-      <div className={ style.logoutContainer }>
-        <Button onClick={ () => loginWithRedirect() } primary>{ t("logout.button.logout") }</Button>
-      </div>
+      <Logo className={ style.logoutLogo } />
+      <div className={ style.logoutTitle }>{ t("logout.title") }</div>
+      <div className={ style.logoutText }>{ t("logout.text") }</div>
+      <Button
+        transparent
+        onClick={ () => loginWithRedirect() }
+        className={ style.logoutBtnLogin  }
+        >{ t("logout.buttonLogin") }</Button>
+      <Button
+        menu
+        href={ ROUTES.ABOUT_TIPCAT.path }
+        className={ style.logoutLinkAbout  }
+      >{ t("logout.LinkAbout") }</Button>
     </div>
   )
 };
